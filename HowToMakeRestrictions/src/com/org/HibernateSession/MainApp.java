fad0ea5625c71378638f924ef612a823384eaf59
Bhawna Budhiraja <Bhawna_Budhiraja@intuit.com>
2017-07-29 10:24:23 +0530

    first commit

---
+++ HowToMakeRestrictions/src/com/org/HibernateSession/MainApp.java
@@ -0,0 +1,34 @@
+package com.org.HibernateSession;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.criterion.Restrictions;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SessionFactory sessionFactory=new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		session.beginTransaction();
+
+		Criteria criteria=session.createCriteria(Student.class);
+		criteria.add(Restrictions.eq("id", 5));
+		
+		List<Student> student=(List<Student>)criteria.list();
+	
+		session.getTransaction().commit();
+		session.close();
+		
+		for(Student s:student)
+		{
+			System.out.println(s.getId()+" "+s.getName());
+		}
+	}
+
+}
